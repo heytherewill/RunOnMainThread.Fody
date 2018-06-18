8788e80ae50f3e1665e421b8485f1da39036ec1b
William Barbosa <william.sb@hotmail.com>
2018-06-18 19:01:11 -0300

    Test

---
+++ RunOnMainThread/Platforms/WPF/MainThreadDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace RunOnMainThread.Platforms.WPF
+{
+    public static class MainThreadDispatcher
+    {
+        public static void RunOnMainThread(Action action)
+        {
+            Application.Current.Dispatcher.BeginInvoke(action);
+        }
+    }
+}
